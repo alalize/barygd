e1ac2a16d40d599b3e8714a1616587c0a5bff2f9
alalize <chiheb.daaloul@ec-m.fr>
2020-07-29 14:47:53 +0200

    gradient clip and convexifier to c cost

--- sampler.py
+++ sampler.py
@@ -12,64 +12,177 @@ from autograd import elementwise_grad as egrad
 import matplotlib.pyplot as plt
 import subprocess
 import utils
+import datetime
 
 
 np.random.seed(0)
+now_str = str(datetime.datetime.now()).replace(' ', '_')
+
+just_lawgd = False
 
 
 # mesh parameters
-n_mesh = 128 
+n_mesh = 128
 mesh_x_min, mesh_x_max = -6, 6
 #mesh = np.ogrid[mesh_x_min:mesh_x_max:complex(0, n_mesh)]
 mesh = np.linspace(mesh_x_min, mesh_x_max, num=n_mesh)
 
 
 # simulation parameters
-bary_coords = [0.9, 0.1]
+bary_coords = [1, 0]
 simul_params = {
-    'n_schemes': 50,
+    'n_schemes': 1,
     'n_eigenvalues': -1,
     'langevin_perturbation': False,
     'langevin_factor': 8e-4,
-    'n_iterations': 10000,
-    'initial_domain': (-1, 1),
+    'n_iterations': 20000,
+    'initial_domain': (4.5, 5),
     'fd_step_size': (mesh_x_max - mesh_x_min) / (n_mesh - 1),
     'tfd_step_size': 1e-6,
-    'gd_step_size': 1e-1,
-    'regularization': 4e-3,
+    'gd_step_size': 1e-2,
+    'regularization': 1,
     'n_samples': 50,
     'bary_coords': bary_coords,
     'n_marginals': len(bary_coords)
 }
+exp_name = now_str + ''.join(['{}:{}_'.format(k[:4], simul_params[k]) for k in simul_params.keys()])
 
+print('NAME:', exp_name)
 print('n_mesh: {}\t fd_step_size: {}'.format(n_mesh, simul_params['fd_step_size']))
 
 
 # definition of marginals
-mean_1, mean_2 = -5, 5
+mean_1, mean_2 = 0, 3
 std_1, std_2 = 1, 1
 marginal_1 = S.norm.pdf(mesh, loc=mean_1, scale=std_1)
 marginal_2 = S.norm.pdf(mesh, loc=mean_2, scale=std_2)
 bary_12 = S.norm.pdf(mesh, loc=bary_coords[0]*mean_1 + mean_2*bary_coords[1], scale=1)
 avg_12 = bary_coords[0]*marginal_1 + bary_coords[1]*marginal_2
+mixture_plt = (marginal_1 + marginal_2)/2
 
 
 # computing kernels for the marginals and their gradients
 potential_1 = lambda x: utils.marginal_potential(x, mean_1, std_1)
 potential_2 = lambda x: utils.marginal_potential(x, mean_2, std_2)
 
+#mixture_12 = lambda x: -autograd_np.log(autograd_np.exp(-(x-mean_1)**2/2) + autograd_np.exp(-(x-mean_2)**2/2))
+#potential_1 = mixture_12
+
 gradient_potential_1 = egrad(potential_1)
 gradient_potential_2 = egrad(potential_2)
+
 
 laplacian_potential_1 = np.vectorize(lambda x: float(hessian(potential_1)(x)))
 laplacian_potential_2 = np.vectorize(lambda x: float(hessian(potential_2)(x)))
 
-kernel_1 = utils.inverse_schrodinger_kernel(mesh, np.vectorize(potential_1), gradient_potential_1, laplacian_potential_1, simul_params['fd_step_size'], n_eigen=simul_params['n_eigenvalues'])
-kernel_2 = utils.inverse_schrodinger_kernel(mesh, np.vectorize(potential_2), gradient_potential_2, laplacian_potential_2, simul_params['fd_step_size'], n_eigen=simul_params['n_eigenvalues'])
+kernel_1 = utils.inverse_schrodinger_kernel(mesh, np.vectorize(potential_1), \
+    gradient_potential_1, laplacian_potential_1, simul_params['fd_step_size'], \
+        n_eigen=simul_params['n_eigenvalues'])
+kernel_2 = utils.inverse_schrodinger_kernel(mesh, np.vectorize(potential_2), \
+    gradient_potential_2, laplacian_potential_2, simul_params['fd_step_size'], \
+        n_eigen=simul_params['n_eigenvalues'])
 
 gradient_kernel_1 = utils.fd_kernel_gradient(kernel_1, simul_params['fd_step_size'])
 gradient_kernel_2 = utils.fd_kernel_gradient(kernel_2, simul_params['fd_step_size'])
 kernel_gradients = [gradient_kernel_1, gradient_kernel_2]
+
+
+def lawgd(mesh, kernel_gradient, simul_params):
+    def lawgd_iteration(L, it):
+        L_new = np.zeros(L.shape)
+
+        mesh_projections = [np.argmin(np.abs(L[j] - mesh)) for j in range(simul_params['n_samples'])]
+        grads = []
+
+        for i in range(simul_params['n_samples']):
+            grad = (simul_params['gd_step_size'] / simul_params['n_samples']) \
+                    * np.sum(kernel_gradient[mesh_projections[i], mesh_projections], axis=0)
+
+            if np.abs(grad) > 2*simul_params['gd_step_size']: grad = np.sign(grad)*simul_params['gd_step_size']
+
+            L_new[i] = L[i] - grad
+
+            grads.append(grad)
+        
+        return L_new, grads
+
+    initial_domain_start, initial_domain_end = simul_params['initial_domain'][0], simul_params['initial_domain'][1]
+    initial_domain_length = initial_domain_end - initial_domain_start
+    X = S.uniform.rvs(loc=initial_domain_start, scale=initial_domain_length, size=simul_params['n_samples']).reshape((-1, 1))
+    X_init = np.copy(X)
+
+    plt.ion()
+    for iteration in range(simul_params['n_iterations']):
+        X, grads = lawgd_iteration(X, iteration)
+
+        if iteration % 500 == 0:
+            plt.clf()
+            plt.title('iteration {}'.format(iteration))
+            plt.grid()
+            plt.scatter(X_init, np.zeros(simul_params['n_samples']), c='b', alpha=0.05)
+            plt.scatter(X, np.zeros(simul_params['n_samples']), c='r', marker='x')
+            plt.scatter(X, grads, c='g', alpha=0.1, marker='o')
+            plt.xlim(mesh.min(), mesh.max())
+            plt.draw()
+            plt.pause(0.001)
+    plt.ioff()
+    plt.close()
+    
+    return X, X_init
+
+
+if just_lawgd:
+    swarm, swarm_init = lawgd(mesh, kernel_gradients[0], simul_params)
+    valid_samples = swarm[np.where(np.logical_and(swarm < mesh.max(), swarm > mesh.min()))[0]]
+    lawgd_density = S.gaussian_kde(valid_samples.reshape((-1,)))
+    print('valid_samples lawgd:', valid_samples.size)
+
+    from scipy.optimize import curve_fit
+
+    def linear_model(x, a, b):
+        return a*x + b
+
+    #points = S.norm.rvs(loc=0, scale=1, size=1000)
+    points = valid_samples
+    hist, bins = np.histogram(points, bins=50, density=True)
+    centers = np.array([0.5*(bins[i] + bins[i+1]) for i in range(50)])
+
+
+    y = np.log(hist)
+    valids = list(set(list(range(y.size))).difference(set(np.where(np.isinf(y))[0].tolist())))
+    x = 0.5*(centers-mean_1)**2
+    x, y = x[valids], y[valids]
+
+    ab_opt, pcov = curve_fit(linear_model, x, y)
+    p_err = round(np.abs((-1) - ab_opt[0]), 2)
+    print('fit parameters: (slope, ord) = {}'.format(ab_opt))
+    print('distnace to normal:', p_err)
+
+    t = np.linspace(x.min(), x.max())
+    y_fit = np.vectorize(lambda x: linear_model(x, ab_opt[0], ab_opt[1]))(t)
+
+
+    fig, axs = plt.subplots(nrows=1, ncols=2)
+
+    axs[0].set_title('fit to normal, slope: {}, fiterr: {}'.format(round(ab_opt[0], 2), p_err))
+    axs[0].scatter(x, y)
+    axs[0].plot(t, y_fit)
+    axs[0].grid()
+
+    axs[1].set_title('lawgd samples')
+    axs[1].scatter(swarm_init, np.zeros(swarm_init.size), c='b', label='initial samples')
+    axs[1].scatter(valid_samples, np.zeros(valid_samples.size), c='r', label='lawgd samples')
+    axs[1].plot(mesh, lawgd_density.pdf(mesh), c='b', alpha=0.7, label='lawgd density')
+    axs[1].plot(mesh, marginal_1, c='b', alpha=0.2, label='target density')
+    axs[1].hist(valid_samples, density=True, color='r', alpha=0.1, bins='auto')
+    #axs[1].plot(mesh, mixture_plt, c='b', alpha=0.2, label='target density')
+    axs[1].grid()
+    axs[1].legend()
+
+    plt.savefig('./img/{}.png'.format(exp_name))
+    plt.show()
+
+    quit(0)
 
 
 # simulation procedure in one dimension
@@ -81,7 +194,13 @@ def bary_gd(mesh, kernel_gradients, simul_params):
             mesh_projections = [np.argmin(np.abs(L[alpha, j] - mesh)) for j in range(simul_params['n_samples'])]
 
             for i in range(simul_params['n_samples']):
-                L_new[alpha, i] = L[alpha, i] - simul_params['gd_step_size']*simul_params['regularization']*simul_params['bary_coords'][alpha]*np.sum(kernel_gradients[alpha][mesh_projections[i], mesh_projections], axis=0) / simul_params['n_samples']
+                L_new[alpha, i] = L[alpha, i] - \
+                    simul_params['gd_step_size'] \
+                        * simul_params['regularization'] \
+                        * simul_params['bary_coords'][alpha] \
+                        * np.sum(kernel_gradients[alpha][mesh_projections[i], mesh_projections], axis=0) \
+                        / simul_params['n_samples']
+
                 if simul_params['langevin_perturbation']:
                     L_new[alpha, i] = L_new[alpha, i] + simul_params['langevin_factor']*np.sqrt(2*simul_params['gd_step_size'])*np.random.normal()
         
@@ -105,7 +224,8 @@ def bary_gd(mesh, kernel_gradients, simul_params):
     initial_domain_start, initial_domain_end = simul_params['initial_domain'][0], simul_params['initial_domain'][1]
     initial_domain_length = initial_domain_end - initial_domain_start
     X = S.uniform.rvs(loc=initial_domain_start, scale=initial_domain_length, size=simul_params['n_marginals']).reshape((-1, 1))
-    L = S.uniform.rvs(loc=initial_domain_start, scale=initial_domain_length, size=simul_params['n_marginals']*simul_params['n_samples']).reshape((simul_params['n_marginals'], simul_params['n_samples'], 1))
+    L = S.uniform.rvs(loc=initial_domain_start, scale=initial_domain_length, size=simul_params['n_marginals']*simul_params['n_samples']) \
+        .reshape((simul_params['n_marginals'], simul_params['n_samples'], 1))
 
     X_init = np.copy(X)
 
--- utils.py
+++ utils.py
@@ -8,6 +8,7 @@ from sklearn.preprocessing import normalize
 from autograd import grad, hessian
 from autograd import elementwise_grad as egrad
 import matplotlib.pyplot as plt
+from numpy.polynomial.hermite_e import *
 
 
 def c(x, lamb):
@@ -19,15 +20,6 @@ def c(x, lamb):
     return tr
 
 
-def fd_c_grad_ij(X, marginal, sample, lamb, epsilon):
-    e_ms = np.zeros(X.shape)
-    e_ms[marginal, sample] = 1
-    fd = (c(X + epsilon*e_ms, lamb) - c(X - epsilon*e_ms, lamb)) * e_ms / (2*epsilon)
-    fd = fd[marginal]
-
-    return fd
-
-
 def fd_c_grad(X, marginal, lamb, epsilon):
     def e_alb(i, j):
         e = np.zeros(X.shape)
@@ -35,24 +27,29 @@ def fd_c_grad(X, marginal, lamb, epsilon):
 
         return e
 
-    grad = np.array([(c(X + epsilon*e_mj, lamb) - c(X - epsilon*e_mj, lamb)) / (2*epsilon) for e_mj in [e_alb(marginal, j) for j in range(X.shape[1])]])
+    grad = np.array([\
+        (c(X + epsilon*e_mj, lamb) - c(X - epsilon*e_mj, lamb)) / (2*epsilon) \
+            for e_mj in [e_alb(marginal, j) \
+            for j in range(X.shape[1])]])
 
     return grad
 
 
 def marginal_potential(X, mu, sigma):
     if not isinstance(X, float):
-        return 0.5 * np.dot((X - mu), (X - mu).T) / (sigma**2)
+        pot = 0.5 * np.dot((X - mu), (X - mu).T) / (sigma**2)
     else:
-        return 0.5 * ((X - mu) / sigma)**2
+        pot = 0.5 * ((X - mu) / sigma)**2
+
+    return pot
 
 
 def schrodinger_potential(X, grad_V, laplacian_V):
     gVX = grad_V(X)
-    if X.ndim > 1 and X.shape[1] > 1:
-        potential = (np.dot(gVX, gVX.T) - 2*laplacian_V(X)) / 4
-    else:
-        potential = (gVX**2 - 2*laplacian_V(X)) / 4
+    #if X.ndim > 1 and X.shape[1] > 1:
+    #    potential = (np.dot(gVX, gVX.T) - 2*laplacian_V(X)) / 4
+    #else:
+    potential = (gVX**2 - 2*laplacian_V(X)) / 4
 
     return potential
 
@@ -71,22 +68,23 @@ def schrodinger_laplacian(X, grad_V, laplacian_V, epsilon):
     return sl_op
 
 
-def inverse_schrodinger_kernel(X, V, grad_V, laplacian_V, epsilon, n_eigen=-1):
+def inverse_schrodinger_kernel(mesh, potential, grad_potential, laplacian_potential, epsilon, n_eigen=-1):
     if n_eigen == -1:
-        n_eigen = X.shape[0] - 1
+        n_eigen = mesh.shape[0] - 1
 
-    L_S = schrodinger_laplacian(X, grad_V, laplacian_V, epsilon)
+    L_S = schrodinger_laplacian(mesh, grad_potential, laplacian_potential, epsilon)
 
-    eta, phi = eigh(L_S)
-    eta, phi = eta[eta > 0], phi.T[eta > 0, :]
-    eta, phi = eta[:n_eigen], phi[:n_eigen, :]
+    eta_S, phi_S = eigh(L_S)
+    eta_S[eta_S < 0] = np.min(eta_S[eta_S > 0])
+    phi_S = phi_S.T
+    eta_S, phi_S = eta_S[:n_eigen], phi_S[:n_eigen]
 
-    rescale = np.repeat(np.exp(V(X)/2).reshape((1, phi.shape[1])), repeats=phi.shape[0], axis=0)
-    phi = rescale * phi
-    eta = inv(np.diag(eta))
-    psi = np.dot(np.sqrt(eta), phi)
+    rescale = np.repeat(np.exp(potential(mesh)/2).reshape((1, -1)), repeats=phi_S.shape[0], axis=0)
+    phi = np.multiply(rescale, phi_S)
+    eta = inv(np.diag(np.sqrt(eta_S)))
+    psi = np.dot(eta, phi)
 
-    K = np.dot(psi.T, psi).T
+    K = np.dot(psi.T, psi)
 
     return K
 
@@ -94,9 +92,9 @@ def inverse_schrodinger_kernel(X, V, grad_V, laplacian_V, epsilon, n_eigen=-1):
 def fd_kernel_gradient(k, epsilon):
     grad_k = np.zeros(k.shape)
 
-    grad_k[0] = (k[1, :] - k[0, :]) / epsilon
-    for i in range(1, k.shape[0]-1):
-        grad_k[i] = (k[i+1, :] - k[i-1, :]) / (2*epsilon)
-    grad_k[-1] = (k[-1, :] - k[-2, :]) / epsilon
+    grad_k[0] = (k[:, 1] - k[:, 0]) / epsilon
+    for i in range(1, k.shape[1]-1):
+        grad_k[i] = (k[:, i+1] - k[:, i-1]) / (2*epsilon)
+    grad_k[-1] = (k[:, -1] - k[:, -2]) / epsilon
 
     return grad_k
